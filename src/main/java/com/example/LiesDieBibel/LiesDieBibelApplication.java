e3f458289f9e57fa81fd19d597cd767ffe278563
Eugen Friesen <eugen.friesen@dwpbank.de>
2022-01-31 11:53:33 +0100

    initial commit

---
+++ src/main/java/com/example/LiesDieBibel/LiesDieBibelApplication.java
@@ -0,0 +1,13 @@
+package com.example.LiesDieBibel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiesDieBibelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiesDieBibelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LiesDieBibel/LiesDieBibelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LiesDieBibel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiesDieBibelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
